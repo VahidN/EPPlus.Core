80d3d9540073d66a81cef13f55e8652b03218c38
VahidN <vahid_nasiri@yahoo.com>
2017-08-28 18:28:52 +0430

    Added NugetPackageTest

---
+++ src/NugetPackageTest/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.IO;
+using OfficeOpenXml;
+using OfficeOpenXml.Table;
+
+namespace NugetPackageTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var package = createExcelPackage())
+            {
+                package.SaveAs(new FileInfo("report.xlsx"));
+            }
+        }
+
+        private static ExcelPackage createExcelPackage()
+        {
+            var package = new ExcelPackage();
+            package.Workbook.Properties.Title = "Salary Report";
+            package.Workbook.Properties.Author = "Vahid N.";
+            package.Workbook.Properties.Subject = "Salary Report";
+            package.Workbook.Properties.Keywords = "Salary";
+
+
+            var worksheet = package.Workbook.Worksheets.Add("Employee");
+
+            //First add the headers
+            worksheet.Cells[1, 1].Value = "ID";
+            worksheet.Cells[1, 2].Value = "Name";
+            worksheet.Cells[1, 3].Value = "Gender";
+            worksheet.Cells[1, 4].Value = "Salary (in $)";
+
+            //Add values
+
+            var numberformat = "#,##0";
+            var dataCellStyleName = "TableNumber";
+            var numStyle = package.Workbook.Styles.CreateNamedStyle(dataCellStyleName);
+            numStyle.Style.Numberformat.Format = numberformat;
+
+            worksheet.Cells[2, 1].Value = 1000;
+            worksheet.Cells[2, 2].Value = "Jon";
+            worksheet.Cells[2, 3].Value = "M";
+            worksheet.Cells[2, 4].Value = 5000;
+            worksheet.Cells[2, 4].Style.Numberformat.Format = numberformat;
+
+            worksheet.Cells[3, 1].Value = 1001;
+            worksheet.Cells[3, 2].Value = "Graham";
+            worksheet.Cells[3, 3].Value = "M";
+            worksheet.Cells[3, 4].Value = 10000;
+            worksheet.Cells[3, 4].Style.Numberformat.Format = numberformat;
+
+            worksheet.Cells[4, 1].Value = 1002;
+            worksheet.Cells[4, 2].Value = "Jenny";
+            worksheet.Cells[4, 3].Value = "F";
+            worksheet.Cells[4, 4].Value = 5000;
+            worksheet.Cells[4, 4].Style.Numberformat.Format = numberformat;
+
+            // Add to table / Add summary row
+            var tbl = worksheet.Tables.Add(new ExcelAddressBase(fromRow: 1, fromCol: 1, toRow: 4, toColumn: 4), "Data");
+            tbl.ShowHeader = true;
+            tbl.TableStyle = TableStyles.Dark9;
+            tbl.ShowTotal = true;
+            tbl.Columns[3].DataCellStyleName = dataCellStyleName;
+            tbl.Columns[3].TotalsRowFunction = RowFunctions.Sum;
+            worksheet.Cells[5, 4].Style.Numberformat.Format = numberformat;
+
+            // AutoFitColumns
+            worksheet.Cells[1, 1, 4, 4].AutoFitColumns();
+
+            return package;
+        }
+    }
+}
